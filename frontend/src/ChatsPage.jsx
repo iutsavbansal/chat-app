9bfd6b205e231f1ed38460052c5169342a19c97b
Utsav Bansal <utsavbansal75@gmail.com>
2024-06-26 14:51:46 +0530

    linking backend with frontend

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,13 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic, useSingleChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic('666fc245-1469-40e7-84b0-68fbe68e84f8', props.user.username, props.user.secret);
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
+        </div>
+    )
 }
 
 export default ChatsPage
